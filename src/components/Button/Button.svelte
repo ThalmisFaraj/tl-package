2257933e0253b40ddc79b6ebe0a9d6a37390dd57
ThalmisFaraj <thalmis.faraj@zaeemsolutions.com>
2025-04-04 12:20:11 +0530

    first commit

---
+++ src/components/Button/Button.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let text = "Click me";
+    export let onClick = () => {};
+</script>
+
+<button
+    on:click={onClick}
+    class="px-4 py-2 rounded-md border border-gray-300 cursor-pointer bg-blue-800 text-white hover:bg-blue-600 transition-colors duration-200"
+>
+    {text}
+</button>
+
+<style>
+</style>
---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve';
+
+  return {
+    plugins: [svelte()],
+    root: isDev ? 'demo' : '.', // only use 'demo' for dev
+    appType: isDev ? 'spa' : 'library',
+    build: {
+      lib: {
+        entry: 'src/index.js',
+        name: 'TlTestPackage',
+        fileName: (format) => `index.${format}.js`
+      },
+      rollupOptions: {
+        external: ['svelte'],
+        output: {
+          globals: {
+            svelte: 'Svelte'
+          }
+        }
+      }
+    },
+    server: {
+      open: true
+    }
+  };
+});
